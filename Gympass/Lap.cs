3c3c64d12c28d1147bb84ed9dbcdbddebb0759cd
Matheus de Mello Romeiro <matheus.romeiro@gmail.com>
2019-04-26 20:37:15 -0300

    Parser for the laps implemented

---
+++ Gympass/Lap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gympass
+{
+    class Lap
+    {
+        int PilotId { get; set; }
+
+        string PilotName { get; set; }
+
+        int LapNumber { get; set; }
+
+        TimeSpan lapTime { get; set; }
+
+        public static Lap ParseLap(string line)
+        {
+            var pilotData = line.Substring(18, 40).TrimEnd();
+
+            var pilotName = pilotData.Split('–')[1].TrimStart();
+            var pilotId = int.Parse(pilotData.Split('–')[0].TrimEnd());
+            var lapNumber = int.Parse(line.Substring(58, 14).TrimEnd());
+            var lapTime = TimeSpan.Parse($"0:0:{line.Substring(72,32).TrimEnd()}", new CultureInfo("en-US"));
+
+            return new Lap()
+            {
+                PilotId = pilotId,
+                PilotName = pilotName,
+                LapNumber = lapNumber,
+                lapTime = lapTime
+            };
+        }
+    }
+}
--- Gympass/Program.cs
+++ Gympass/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace Gympass
     {
         static void Main(string[] args)
         {
+            string line;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please inform the name of the input file");
+                Environment.Exit(1);
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"File {args[0]} does not exist.");
+                Environment.Exit(1);
+            }
+
+            var file = new StreamReader(args[0]);
+            line = file.ReadLine();
+            while ((line = file.ReadLine()) != null)
+            {
+                var lap = Lap.ParseLap(line);
+            }
+
+            Environment.Exit(0);
         }
     }
 }
